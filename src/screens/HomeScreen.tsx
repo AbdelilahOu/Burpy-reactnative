16a9856b6774dff8ff3234a9da9fc2523ec11e9d
Abdelilah_OU <gxgamer710@gmail.com>
2023-03-08 22:03:38 +0000

    adding border radius to image

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -7,6 +7,7 @@ import {
   useImage,
 } from '@shopify/react-native-skia';
 import {Pressable, Text, useWindowDimensions, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 
 export const HomeScreen = () => {
   const homeImage = useImage(require('../assets/home.jpg'));
@@ -14,24 +15,9 @@ export const HomeScreen = () => {
   const roundedRect = rrect(rect(0, 0, width - 20, height * 0.75 - 20), 10, 10);
 
   return (
-    <View
-      style={{
-        height: height,
-        width: width,
-        backgroundColor: '#fff',
-      }}>
-      <View
-        style={{
-          width: width,
-          height: '75%',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Canvas
-          style={{
-            width: width - 20,
-            height: height * 0.75 - 20,
-          }}>
+    <View style={{...homeStyle.main, height, width}}>
+      <View style={{...homeStyle.image, width}}>
+        <Canvas style={{width: width - 20, height: height * 0.75 - 20}}>
           <Group clip={roundedRect}>
             {homeImage && (
               <Image
@@ -47,36 +33,45 @@ export const HomeScreen = () => {
         </Canvas>
       </View>
       <View style={{width: '100%', height: '15%'}}></View>
-      <View
-        style={{
-          width: '100%',
-          height: '10%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          padding: 10,
-        }}>
-        <Pressable
-          style={{
-            width: '100%',
-            height: 55,
-            borderRadius: 10,
-            backgroundColor: '#00BBF0',
-            display: 'flex',
-            justifyContent: 'center',
-            alignContent: 'center',
-          }}>
-          <Text
-            style={{
-              color: '#fff',
-              textAlign: 'center',
-              fontSize: 20,
-              fontWeight: '500',
-            }}>
-            Get started
-          </Text>
+      <View style={homeStyle.ButtonParent}>
+        <Pressable style={homeStyle.Button}>
+          <Text style={homeStyle.ButtonText}>Get started</Text>
         </Pressable>
       </View>
     </View>
   );
 };
+
+const homeStyle = StyleSheet.create({
+  main: {
+    backgroundColor: '#fff',
+  },
+  image: {
+    height: '75%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ButtonParent: {
+    width: '100%',
+    height: '10%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  Button: {
+    width: '100%',
+    height: 55,
+    borderRadius: 10,
+    backgroundColor: '#00BBF0',
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  ButtonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '500',
+  },
+});
